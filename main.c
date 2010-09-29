4882e6b0b5284e55ddc58e466a2cb2551e49462e
Dario <darios3@gmail.com>
2010-09-29 00:29:33 -0300

    added print of results in main.c

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include "ruby.h"
 
 
+VALUE error_catch(VALUE parm, VALUE err) {
+
+	VALUE tos = rb_inspect(err);
+	fprintf(stderr, "%s\n", RSTRING(tos)->ptr);
+	
+	return Qnil;
+}
+
 int main(int argc, char* argv[] ) {
 	{
 		RUBY_INIT_STACK
 		ruby_init();
 		
-		rb_rescue( rb_eval_string, (VALUE)argv[1], 0,0);
+		VALUE result = rb_rescue( rb_eval_string, (VALUE)argv[1], error_catch,0);
+		
+		VALUE rb_result = rb_inspect(result);
+		fprintf(stderr, "%s\n", RSTRING(rb_result)->ptr);
 	    }
 	return 0;
 }
